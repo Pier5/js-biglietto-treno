a1a32f108401b240fd77ebbca28e10a64fd2814e
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-21 15:50:19 +0100

    calcolo con gli sconti

--- js/myscript.js
+++ js/myscript.js
@@ -2,8 +2,17 @@ let km = prompt ('Quanti km devi percorrere?');
 let age = prompt ('Quanti anni hai?');
 let kmPrice = 0.21;
 let totalPrice = kmPrice * km;
+let price = totalPrice 
 
 document.getElementById('my-output').innerHTML = totalPrice;
+
+if (age < 18) {
+    let price = totalPrice * 80 / 100;
+    document.getElementById('my-output').innerHTML = price;
+} else if (age > 65) {
+    let price = totalPrice * 60 / 100;
+    document.getElementById('my-output').innerHTML = price;
+}
 
 
 
