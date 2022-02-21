60604155228bb295ac80d84c5d42f5f9f2111e8f
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-21 15:25:46 +0100

    inizio js -calcolo prezzo per km-

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+let km = prompt ('Quanti km devi percorrere?');
+let age = prompt ('Quanti anni hai?');
+let kmPrice = 0.21;
+let totalPrice = kmPrice * km;
+
+document.getElementById('my-output').innerHTML = totalPrice;
+
+
+
+
+
+
