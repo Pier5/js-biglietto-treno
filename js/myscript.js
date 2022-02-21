bc302fb455ebb0813ec04958dc2e666151e5f65b
Piergiorgio Piras <piraspiergiorgio5@gmail.com>
2022-02-21 16:36:44 +0100

    inserito il toFixed per risulato finale con max due decimali

--- js/myscript.js
+++ js/myscript.js
@@ -2,16 +2,16 @@ let km = prompt ('Quanti km devi percorrere?');
 let age = prompt ('Quanti anni hai?');
 let kmPrice = 0.21;
 let totalPrice = kmPrice * km;
-let price = totalPrice 
+let price = totalPrice;
 
 document.getElementById('my-output').innerHTML = totalPrice;
 
 if (age < 18) {
-    let price = totalPrice * 80 / 100;
-    document.getElementById('my-output').innerHTML = price;
+    price = totalPrice * 80 / 100;
+    document.getElementById('my-output').innerHTML = price.toFixed(2);
 } else if (age > 65) {
-    let price = totalPrice * 60 / 100;
-    document.getElementById('my-output').innerHTML = price;
+    price = totalPrice * 60 / 100;
+    document.getElementById('my-output').innerHTML = price.toFixed(2);
 }
 
 
